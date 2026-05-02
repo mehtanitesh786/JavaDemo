eb0e27f0a562788250c1abbfec5d6551031beba7
Nitesh <mehtanitesh786@gmail.com>
2026-05-02 23:39:01 +0530

    Added

--- src/comparablevscomparator/CompareToDemo.java
+++ src/comparablevscomparator/CompareToDemo.java
@@ -11,10 +11,20 @@ public class CompareToDemo {
         t.add(new StringBuffer("Z"));
         t.add(new StringBuffer("K"));
 
+        TreeSet t1= new TreeSet();
+        t1.add("a");
+        t1.add("Z");
+        t1.add("L");
+        t1.add("A");
+        System.out.println(t1);
+
         System.out.println(t); //earlier version StringBuffer not implement comparable but in 24 version it will implement
         System.out.println("A".compareTo("Z"));
         System.out.println("Z".compareTo("B"));
         System.out.println("A".compareTo("A"));
         System.out.println("A".compareTo(null));
+
+
+
     }
 }
